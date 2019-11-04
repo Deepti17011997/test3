c5295d88fd43334c102deb98960758ea22de223f
Deepti Tamhane <DEEPTI.TAMHANE@LNTINFOTECH.COM>
2019-11-04 11:43:30 +0530

    completedddd

--- TestJavaPr/src/pack/Main.java
+++ TestJavaPr/src/pack/Main.java
@@ -5,6 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("in new weclipse file to be uplaoded in github");
-	}
+System.out.println("icompleted");}
 
 }
